61e5e91ddd06a15a8eee90bbe4dad774d44b8d81
Shiny Master of all <101048219+Shiny-Miner@users.noreply.github.com>
2024-07-14 01:56:34 +0530

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -1419,13 +1419,14 @@ const u16 gUnknown_8EAFF60[] = INCBIN_U16("graphics/unknown/unknown_EAFF60.gbapa
 const u32 gUnknown_8EAFFC0[] = INCBIN_U32("graphics/berry_crush/berry_crush.4bpp.lz");
 const u32 gUnknown_8EB0ADC[] = INCBIN_U32("graphics/berry_crush/data_EB0ADC.bin.lz");
 
-//Option Menu
-const u16 gPalOptionMenuText[] = INCBIN_U16("graphics/option_menu/text.gbapal");
-const u16 gPalOptionMenuButtons[] = INCBIN_U16("graphics/option_menu/buttons.gbapal");
-const u16 gPalOptionMenuNoSel[] = INCBIN_U16("graphics/option_menu/no_sel.gbapal");
-const u16 gPalOptionMenuSel[] = INCBIN_U16("graphics/option_menu/sel.gbapal");
-const u32 gTilesOptionMenu[] = INCBIN_U32("graphics/option_menu/tiles.4bpp.lz");
-const u32 gMapOptionMenuButtons[] = INCBIN_U32("graphics/option_menu/buttons.bin.lz");
-const u32 gMapOptionMenuBG[] = INCBIN_U32("graphics/option_menu/bg.bin.lz");
-const u32 gMapOptionMenuFrame[] = INCBIN_U32("graphics/option_menu/frame.bin.lz");
-
+//Main Menu
+const u16 sMainMenuTextPal[] = {RGB(0, 0, 0), RGB(30, 30, 30), RGB(17, 17, 17), RGB(7, 20, 31), RGB(7, 13, 20)};
+const u16 sMainMenuTextFemalePal[] = {RGB(31, 9, 11), RGB(21, 8, 8)};
+const u16 gPalMainMenuBG[] = INCBIN_U16("graphics/main_menu/bg.gbapal");
+const u16 gPalMainMenuNoSel[] = INCBIN_U16("graphics/main_menu/no_sel.gbapal");
+const u16 gPalMainMenuSel[] = INCBIN_U16("graphics/main_menu/sel.gbapal");
+const u32 gTilesMainMenuBG1[] = INCBIN_U32("graphics/main_menu/tiles_bg1.4bpp.lz");
+const u32 gTilesMainMenuBG2[] = INCBIN_U32("graphics/main_menu/tiles_bg2.4bpp.lz");
+const u32 gMapMainMenuBG2[] = INCBIN_U32("graphics/main_menu/bg.bin.lz");
+const u32 gMapMainMenuNewGame[] = INCBIN_U32("graphics/main_menu/new_game.bin.lz");
+const u32 gMapMainMenuContinue[] = INCBIN_U32("graphics/main_menu/continue.bin.lz");
