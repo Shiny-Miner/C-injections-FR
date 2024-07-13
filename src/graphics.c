8d2fb112e77dacfbf92e8ae0722ed2432e3e87e3
Shiny Master of all <101048219+Shiny-Miner@users.noreply.github.com>
2024-07-14 03:54:01 +0530

    Update graphics.c

--- src/graphics.c
+++ src/graphics.c
@@ -3,7 +3,7 @@
 //Main Menu
 const u16 sMainMenuTextPal[] = {RGB(0, 0, 0), RGB(30, 30, 30), RGB(17, 17, 17), RGB(7, 20, 31), RGB(7, 13, 20)};
 const u16 sMainMenuTextFemalePal[] = {RGB(31, 9, 11), RGB(21, 8, 8)};
-const u16 gPalMainMenuBG[] = INCBIN_U16("graphics/bg.gbapal");
+const u16 gPalMainMenuBG[] = INCBIN_U16("graphics/bg.pal");
 const u16 gPalMainMenuNoSel[] = INCBIN_U16("graphics/no_sel.gbapal");
 const u16 gPalMainMenuSel[] = INCBIN_U16("graphics/sel.gbapal");
 const u32 gTilesMainMenuBG1[] = INCBIN_U32("graphics/tiles_bg1.4bpp.lz");
