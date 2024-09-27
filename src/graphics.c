d9cd3955893dcf597537ce897a3431e62ce7e0ad
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-09-28 02:20:19 +0530

    Create graphics.c

---
+++ src/graphics.c
@@ -0,0 +1,23 @@
+#include "global.h"
+// Pokédex
+const u16 gPokedexBgHoenn_Pal[] = INCBIN_U16("graphics/bg_hoenn.gbapal");
+const u16 gPokedexSearchResults_Pal[] = INCBIN_U16("graphics/search_results_bg.gbapal");
+const u16 gPokedexBgNational_Pal[] = INCBIN_U16("graphics/bg_national.gbapal");
+const u32 gPokedexMenu_Gfx[] = INCBIN_U32("graphics/menu.4bpp.lz");
+const u32 gPokedexInterface_Gfx[] = INCBIN_U32("graphics/interface.4bpp.lz");
+const u32 gUnused_PokedexNoBall[] = INCBIN_U32("graphics/noball_unused.4bpp.lz");
+const u32 gPokedexStartMenuMain_Tilemap[] = INCBIN_U32("graphics/start_menu_main.bin.lz");
+const u32 gPokedexStartMenuSearchResults_Tilemap[] = INCBIN_U32("graphics/start_menu_search_results.bin.lz");
+const u32 gPokedexList_Tilemap[] = INCBIN_U32("graphics/list.bin.lz");
+const u32 gPokedexListUnderlay_Tilemap[] = INCBIN_U32("graphics/list_underlay.bin.lz");
+const u32 gPokedexSizeScreen_Tilemap[] = INCBIN_U32("graphics/size_screen.bin.lz");
+const u32 gPokedexScreenSelectBarMain_Tilemap[] = INCBIN_U32("graphics/screen_select_bar_main.bin.lz");
+const u32 gPokedexScreenSelectBarSubmenu_Tilemap[] = INCBIN_U32("graphics/screen_select_bar_submenu.bin.lz");
+const u32 gPokedexInfoScreen_Tilemap[] = INCBIN_U32("graphics/info_screen.bin.lz");
+const u32 gPokedexCryScreen_Tilemap[] = INCBIN_U32("graphics/cry_screen.bin.lz");
+const u16 gPokedexSearchMenu_Pal[] = INCBIN_U16("graphics/search_menu.gbapal");
+const u32 gPokedexSearchMenu_Gfx[] = INCBIN_U32("graphics/search_menu.4bpp.lz");
+const u32 gPokedexSearchMenuNational_Tilemap[] = INCBIN_U32("graphics/search_menu_national.bin.lz");
+const u32 gPokedexSearchMenuHoenn_Tilemap[] = INCBIN_U32("graphics/search_menu_hoenn.bin.lz");
+const u16 gPokedexAreaScreenAreaUnknown_Pal[] = INCBIN_U16("graphics/area_unknown.gbapal");
+const u32 gPokedexAreaScreenAreaUnknown_Gfx[] = INCBIN_U32("graphics/area_unknown.4bpp.lz");
