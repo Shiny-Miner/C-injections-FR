fa7314c1f4dfaf99e7a6b16be749ef931ce6e104
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-10-19 20:32:32 +0530

    Create strings.c

---
+++ src/strings.c
@@ -0,0 +1,16 @@
+#include "global.h"
+#include "strings.h"
+
+const u8 gText_CryOf[] = _("CRY OF");
+const u8 gText_SizeComparedTo[] = _("SIZE COMPARED TO ");
+const u8 gText_PokedexRegistration[] = _("POKéDEX registration completed.");
+const u8 gText_NumberClear01[] = _("{NO}{CLEAR 0x01}");
+const u8 gText_5MarksPokemon[] = _("????? POKéMON");
+const u8 gText_UnkHeight[] = _("{CLEAR_TO 0x0C}??'??”");
+const u8 gText_UnkWeight[] = _("????.? lbs.");
+const u8 gText_HTHeight[] = _("HT");
+const u8 gText_WTWeight[] = _("WT");
+const u8 gText_SearchingPleaseWait[] = _("Searching…\nPlease wait.");
+const u8 gText_SearchCompleted[] = _("Search completed.");
+const u8 gText_NoMatchingPkmnWereFound[] = _("No matching POKéMON were found.");
+const u8 gText_SelectorArrow[] = _("▶");
