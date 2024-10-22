af6bf82646376c46d5d37b340a8f9138dd9acd3a
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2024-10-22 18:07:40 +0530

    Create strings.c

---
+++ src/strings.c
@@ -0,0 +1,22 @@
+#include "global.h"
+#include "strings.h"
+
+const u8 gText_8419B44[] = _("It looks like this\nEgg will take a\nlong time to hatch.");
+const u8 gText_8419B7B[] = _("What will hatch\nfrom this? It will\ntake some time.");
+const u8 gText_8419BAE[] = _("It occasionally\nmoves. It should\nhatch soon.");
+const u8 gText_8419BDB[] = _("It's making sounds.\nIt's almost ready\nto hatch!");
+const u8 gText_8419B18[] = _("A peculiar Pokémon Egg obtained\nin a trade.");
+const u8 gText_8419A3D[] = _("An odd Pokémon Egg found by the\nDay-Care couple.");
+const u8 gText_8419A6E[] = _("An odd Pokémon Egg obtained from\nthe traveling man.");
+const u8 gText_8419AA2[] = _("A wondrously peculiar Pokémon Egg\nobtained at a nice place.");
+const u8 gText_8419ADE[] = _("An odd Pokémon Egg obtained from\nan old woman at the Spa.");
+
+const u8 gText_8419A3DSpa[] = _("Extraño Huevo hallado por\nla pareja de la GUARdeRÍA.");
+const u8 gText_8419A6ESpa[] = _("Extraño Huevo regalo del viajante.");
+const u8 gText_8419AA2Spa[] = _("Extraño Huevo de Pokémon\nhallado en un lugar bonito.");
+const u8 gText_8419ADESpa[] = _("Huevo de Pokémon hallado\nen las fuentes termales.");
+const u8 gText_8419B18Spa[] = _("Extraño Huevo de Pokémon\nrecibido por intercambio.");
+const u8 gText_8419B44Spa[] = _("Este Huevo \ntardará mucho\nen abrirse.");
+const u8 gText_8419B7BSpa[] = _("¿Qué habrá\ndentro? Tendrás\nque esperar más.");
+const u8 gText_8419BAESpa[] = _("A veces se \nmueve. Debe de\nestar a punto.");
+const u8 gText_8419BDBSpa[] = _("Se oyen ruidos\ndentro. ¡Pronto\nse abrirá!");
