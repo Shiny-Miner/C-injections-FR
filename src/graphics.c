ece933f532bea0a2fdaf2ed4988ddea106191ba5
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2025-01-19 18:00:23 +0530

    Update and rename hi to graphics.c

---
+++ src/graphics.c
@@ -0,0 +1,8 @@
+#include "global.h"
+
+//Party Menu
+const u32 gPartyMenuBg_Gfx[] = INCBIN_U32("graphics/party_menu/tiles.4bpp.lz");
+const u32 gPartyMenuBg_Pal[] = INCBIN_U32("graphics/party_menu/palette.gbapal.lz");
+const u32 gPartyMenuBg_Tilemap[] = INCBIN_U32("graphics/party_menu/tilemap.bin.lz");
+const u32 gPartyMenuStatusGfx_Icons[] = INCBIN_U32("graphics/party_menu/status_icons.4bpp.lz");
+const u32 gPartyMenuStatusPal_Icons[] = INCBIN_U32("graphics/party_menu/status_icons.gbapal.lz");
