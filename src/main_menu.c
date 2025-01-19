5cead3182802aeaf8a23c9e26a10777574ca7a5e
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2025-01-19 17:47:20 +0530

    decapitalise

--- src/main_menu.c
+++ src/main_menu.c
@@ -32,11 +32,11 @@
 #include "naming_screen.h"
 #include "string_util.h"
 
-const u8 gText_Options[] = _("OPTION");
+const u8 gText_Options[] = _("Option");
 const u8 gText_Team[] = _("Team: "); 
-const u8 gText_Badges[] = _("BADGES: ");
-const u8 gText_Time[] = _("TIME: "); 
-const u8 gText_Pokedex[] = _("POKEDEX: ");
+const u8 gText_Badges[] = _("Badges: ");
+const u8 gText_Time[] = _("Play Time: "); 
+const u8 gText_Pokedex[] = _("Pokedex: ");
 enum menuAction
 {
 	NEWGAME,
