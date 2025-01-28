9b8d2b2d501eef3f8553ad425f446cca593f1d59
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2025-01-29 04:55:23 +0530

    Update Supreme.c

--- src/Supreme.c
+++ src/Supreme.c
@@ -1,6 +1,23 @@
 #include "global.h"
 #include "strings.h"
+#include "constants/songs.h"
+#include "constants/weather.h"
+#include "constants/rgb.h"
+#include "util.h"
+#include "event_object_movement.h"
 #include "field_weather.h"
+#include "main.h"
+#include "menu.h"
+#include "palette.h"
+#include "random.h"
+#include "script.h"
+#include "start_menu.h"
+#include "sound.h"
+#include "sprite.h"
+#include "task.h"
+#include "trig.h"
+#include "gpu_regs.h"
+#include "field_camera.h"
 
 const u8 gText_AM[] = _("AM");
 const u8 gText_PM[] = _("PM");
