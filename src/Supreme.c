4e35ca39791a0c452c9b816d94e59617f8ecd922
Lord of Shines <101048219+Shiny-Miner@users.noreply.github.com>
2025-01-29 04:52:21 +0530

    Update Supreme.c

--- src/Supreme.c
+++ src/Supreme.c
@@ -1,5 +1,13 @@
 #include "global.h"
 #include "strings.h"
+#include "field_weather.h"
 
 const u8 gText_AM[] = _("AM");
 const u8 gText_PM[] = _("PM");
+bool8 IsWeatherAlphaBlend(void)
+{
+    return gWeatherPtr->currWeather == WEATHER_FOG_HORIZONTAL || 
+           gWeatherPtr->currWeather == WEATHER_FOG_DIAGONAL   ||
+           gWeatherPtr->currWeather == WEATHER_UNDERWATER_BUBBLES ||
+           gWeatherPtr->currWeather == WEATHER_UNDERWATER;
+}
